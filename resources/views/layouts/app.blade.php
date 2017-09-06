07c27ba568b013c065803cfac68b57f36151f334
Adriano <adriano_vianna@outlook.com>
2017-09-06 15:29:39 -0300

    fix layout auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,9 @@
                         <li>
                             <a href="{{ route('logout') }}"
                                onclick="event.preventDefault();
-document.getElementById('logout-form').submit();">
+
+
+                                                     document.getElementById('logout-form').submit();">
                                 Logout
                             </a>
 
@@ -77,12 +79,42 @@ document.getElementById('logout-form').submit();">
 <div class="container">
     <div class="section">
         <div class="container">
+
             @yield('content')
+
+            <div class="fixed-action-btn">
+                <a class="btn-floating btn-large red waves-effect waves-light btn modal-trigger pulse" href="#modal1">
+                    <i class="large material-icons">add</i>
+                </a>
+            </div>
+
+            <div id="modal1" class="modal">
+                <div class="modal-content">
+                    <h4>Criar Desabafo</h4>
+                    <div class="row">
+                        <form class="col s12">
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input id="password" type="password" class="validate">
+                                    <label for="password">Título</label>
+                                </div>
+                                <div class="input-field col s12">
+                                    <input id="email" type="email" class="validate">
+                                    <label for="email">Desabafo</label>
+                                </div>
+                            </div>
+                            <button class="btn waves-effect waves-light right" type="submit" name="action">Publicar
+
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Fechar</a>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-
-
 </div>
 
 {{--<footer class="page-footer blue lighten-2">--}}
