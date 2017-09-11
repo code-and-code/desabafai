4262b9b2058e578592d543618441000c28203366
Adriano <adriano_vianna@outlook.com>
2017-09-11 14:52:25 -0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,37 +87,39 @@
 
             @yield('content')
 
-            <div class="fixed-action-btn">
-                <a class="btn-floating btn-large red waves-effect waves-light btn modal-trigger pulse" href="#modal1">
-                    <i class="large material-icons">add</i>
-                </a>
-            </div>
+           @auth
+                <div class="fixed-action-btn">
+                    <a class="btn-floating btn-large red waves-effect waves-light btn modal-trigger pulse" href="#modal1">
+                        <i class="large material-icons">add</i>
+                    </a>
+                </div>
 
-            <div id="modal1" class="modal">
-                <div class="modal-content">
-                    <h4>Criar Desabafo</h4>
-                    <div class="row">
-                        <form class="col s12">
-                            <div class="row">
-                                <div class="input-field col s12">
-                                    <input id="password" type="password" class="validate">
-                                    <label for="password">Título</label>
+                <div id="modal1" class="modal">
+                    <div class="modal-content">
+                        <h4>Criar Desabafo</h4>
+                        <div class="row">
+                            <form class="col s12">
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <input id="password" type="password" class="validate">
+                                        <label for="password">Título</label>
+                                    </div>
+                                    <div class="input-field col s12">
+                                        <input id="email" type="email" class="validate">
+                                        <label for="email">Desabafo</label>
+                                    </div>
                                 </div>
-                                <div class="input-field col s12">
-                                    <input id="email" type="email" class="validate">
-                                    <label for="email">Desabafo</label>
-                                </div>
-                            </div>
-                            <button class="btn waves-effect waves-light right" type="submit" name="action">Publicar
+                                <button class="btn waves-effect waves-light right" type="submit" name="action">Publicar
 
-                            </button>
-                        </form>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Fechar</a>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Fechar</a>
-                </div>
-            </div>
+            @endauth
         </div>
     </div>
 </div>
