9bab58b7a7d371351ad3b706b751772ae6cfe84b
Adriano <adriano_vianna@outlook.com>
2017-09-18 15:14:59 -0300

    fix app post

--- resources/views/post/posts.blade.php
+++ resources/views/post/posts.blade.php
@@ -81,21 +81,21 @@
                         @foreach($comment->comments->take(3) as $answer)
 
                             <li class="collection-item avatar" style="margin-left: 60px">
-                                <img src="{{ config('avatar.150')}}{{$comment->User->nickname}}" alt="" class="circle">
-                                <span class="title"><a href="/{{$comment->User->nickname}}"> {{$comment->User->nickname}}</a></span>
+                                <img src="{{ config('avatar.150')}}{{$answer->User->nickname}}" alt="" class="circle">
+                                <span class="title"><a href="/{{$answer->User->nickname}}"> {{$answer->User->nickname}}</a></span>
                                 <p></p>
                                 <div class="row " id="respostas">
                                     <div class="col s11">
-                                        <p> <span class="teal-text accent-3"></span>{{$comment->body}} </p>
+                                        <p> <span class="teal-text accent-3"></span>{{$answer->body}} </p>
 
-                                        <a class="like blue-text tooltipped" href="{{route('like.store.comment',$comment)}}" data-position="bottom" data-delay="50" data-tooltip="Curtir"><i class=" material-icons tiny">thumb_up</i></a>
+                                        <a class="like blue-text tooltipped" href="{{route('like.store.comment',$answer)}}" data-position="bottom" data-delay="50" data-tooltip="Curtir"><i class=" material-icons tiny">thumb_up</i></a>
 
-                                        <a class="like red-text tooltipped" href="{{route('denunciation.store.comment',$comment)}}"  data-position="bottom" data-delay="50" data-tooltip="Denunciar"><i class="material-icons tiny">do_not_disturb_alt</i></a>
+                                        <a class="like red-text tooltipped" href="{{route('denunciation.store.comment',$answer)}}"  data-position="bottom" data-delay="50" data-tooltip="Denunciar"><i class="material-icons tiny">do_not_disturb_alt</i></a>
 
                                         <a class="like grey-text tooltipped" href="#" data-position="bottom" data-delay="50" data-tooltip="Excluir"><i class="material-icons tiny">delete_sweep</i></a>
                                     </div>
                                 </div>
-                                <p class="secondary-content">{{$comment->created_at->diffForHumans()}}</p>
+                                <p class="secondary-content">{{$answer->created_at->diffForHumans()}}</p>
                             </li>
 
                         @endforeach
