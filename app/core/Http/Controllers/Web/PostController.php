786df9474a01d53b7dc4a8bd25bf93dc9f61fb30
Angelo Biscola <angelo.biscola@gmail.com>
2017-09-28 15:31:12 -0300

    fix

--- app/core/Http/Controllers/Web/PostController.php
+++ app/core/Http/Controllers/Web/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
 
     public function __construct(PostService $postService)
     {
-        $this->middleware('auth:web')->except(['show']);
+        $this->middleware('auth:web',['except' => ['show']]);
         $this->postService     = $postService;
     }
 
